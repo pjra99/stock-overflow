8a8ea8dc51a9ef979c90938b21207d0b3cda9dba
pjra99 <pranjalbhs@gmail.com>
2022-04-09 16:00:50 +0530

    update: completed login page

--- src/Screens/SignIn.js
+++ src/Screens/SignIn.js
@@ -1,7 +1,64 @@
+import { useState } from "react";
+
 function SignIn() {
+  const [mail, setMail] = useState("");
+  const [pass, setPass] = useState("");
+
+  function Validate() {
+    if (mail == "" && pass == "") {
+      alert("Email/password can't be empty!");
+      return;
+    }
+    if (mail == "") {
+      alert("Email Required!");
+      return;
+    }
+    if (pass == "") {
+      alert("Password Required!");
+      return;
+    }
+    alert("Successful Login!");
+  }
   return (
-    <div className="flex justify-center">
-      <div className="w-1/3 mt-20 h-52 border-black border-2 rounded"></div>
+    <div className="bg-background h-screen">
+      <div className="text-center pt-10 text-xl italic">Stock Overflow</div>
+      <div className="justify-center flex">
+        <div className="w-1/3 mt-24 h-80 rounded bg-white shadow-lg">
+          <div className="flex justify-center py-6 border-b border-lightgray text-xl">
+            Login
+          </div>
+          <div className="flex justify-center pr-2 mt-8">
+            <input
+              type="email"
+              className="border border-gray w-2/3 py-2 my-1 rounded"
+              placeholder=" Email"
+              id="email"
+              value={mail}
+              onChange={(e) => setMail(e.target.value)}
+            />{" "}
+          </div>
+          <div className="flex justify-center mt-3  pr-2">
+            <input
+              type="password"
+              className="password border border-gray w-2/3 py-2 rounded"
+              placeholder=" Password"
+              id="password"
+              value={pass}
+              onChange={(e) => setPass(e.target.value)}
+            />
+          </div>
+          <div className="flex justify-center mt-10">
+            <button
+              className="px-4 py-2 bg-green rounded text-white"
+              onClick={() => {
+                Validate();
+              }}
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
